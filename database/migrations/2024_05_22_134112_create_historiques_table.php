7a4f7c6c70c6183d70ea215c2d59fe0ab459a27e
eelbe029 <146129886+eelbe029@users.noreply.github.com>
2024-05-30 13:01:09 +0000

    Minor change on the migrations (+ active trait in the historique table)

--- database/migrations/2024_05_22_134112_create_historiques_table.php
+++ database/migrations/2024_05_22_134112_create_historiques_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->timestamps();
             $table->date('DateAssignement');
             $table->date('DateDissociation');
+            $table->boolean('active');
             $table->unsignedBigInteger('articleId');
             $table->unsignedBigInteger('personnelId');
 
