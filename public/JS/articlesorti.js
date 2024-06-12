3fd10f633d43bc22fac09f8b521fd1d7727df489
eelbe029 <146129886+eelbe029@users.noreply.github.com>
2024-06-12 09:54:31 +0000

    Working on the last page

---
+++ public/JS/articlesorti.js
@@ -0,0 +1,3 @@
+const el4 = document.getElementById('el4')
+el4.classList.remove('text-white')
+el4.classList.add('active')
--- resources/views/articlesorti.blade.php
+++ resources/views/articlesorti.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h4 class="h2">Articles en stock</h4>
+        <h4 class="h2">Articles sorti du stock</h4>
         <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
                 <button type="button" class="btn btn-sm btn-outline-secondary">blank</button>
@@ -25,6 +25,8 @@
                     <th>Marque</th>
                     <th>Model</th>
                     <th>Type</th>
+                    <th>Emplacement</th>
+                    <th>Nom</th>
                     <th>Actions</th>
                 </tr>
                 </thead>
