e8f311350ccbbb868a12bfe0446a00dd939cecb1
eelbe029 <146129886+eelbe029@users.noreply.github.com>
2024-06-12 10:54:08 +0000

    Making the assignement button work

--- app/Http/Controllers/GestionEmp.php
+++ app/Http/Controllers/GestionEmp.php
@@ -84,48 +84,26 @@ class GestionEmp extends Controller
     }
 
     #[NoReturn] public function assign(Request $request){
-        $personnel = Personnel::where('nom', $request->nom)
-                                ->where('emplacement', $request->emplacement)
-                                ->count();
 
         //Passe l'etat de l'article de enstock a assignee
         $article = Article::findOrFail($request->articleID);
-        $article->etat = 'assigned';
+        $article->etat = 'sorti';
         $article->save();
 
-        //Verifie si le personnel entree existe ou non
-        if ($personnel == 1) {
+        //Recupere les donnees du personnel a qui on associe
+        $personnel = Personnel::where('nom', $request->nom)
+                    ->where('emplacement', $request->emplacement)
+                    ->get();
 
-            //Recupere les donnees du personnel a qui on associe
-            $personnel = Personnel::where('nom', $request->nom)
-                        ->where('emplacement', $request->emplacement)
-                        ->get();
+        //Creer l'historique liee a l'association
+        $historique = new Historique();
+        $historique->DateAssignement = now();
+        $historique->articleId = $article->id;
+        $historique->personnelId = $personnel[0]->id;
+        $historique->DateDissociation = now();
+        $historique->active = true;
+        $historique->save();
 
-            //Creer l'historique liee a l'association
-            $historique = new Historique();
-            $historique->DateAssignement = now();
-            $historique->articleId = $article->id;
-            $historique->personnelId = $personnel[0]->id;
-            $historique->DateDissociation = now();
-            $historique->active = true;
-            $historique->save();
-
-        } else {
-            //Creer un nouveau personnel s'il n'existait pas
-            $personnel = new Personnel();
-            $personnel->nom = $request->nom;
-            $personnel->emplacement = $request->emplacement;
-            $personnel->save();
-
-            //Creer l'historique liee a l'association
-            $historique = new Historique();
-            $historique->DateAssignement = now();
-            $historique->articleId = $article->id;
-            $historique->personnelId = $personnel->id;
-            $historique->DateDissociation = now();
-            $historique->active = true;
-            $historique->save();
-        }
         return view('articlestock');
     }
     public function autocompletename(Request $request): JsonResponse
@@ -180,7 +158,9 @@ class GestionEmp extends Controller
     }
     public function articleSortiData(Request $request): JsonResponse
     {
-        $collection = Article::select('id','marqueId','Model','typeId');
+        $collection = Article::select('id','marqueId','Model','typeId')
+                                ->where('etat','sorti')
+                                ->get();
 
         return
             DataTables::of($collection)
@@ -189,6 +169,9 @@ class GestionEmp extends Controller
                 })
                 ->editColumn("type",function($article){
                     return $article->type->name;
+                })
+                ->editColumn("emplacement",function($article){
+                    return $article->historique->personnelId;
                 })
                 ->addColumn('actions', function ($row) {
                     return '<button  type="button" class=" assign btn btn-primary" data-id="'.$row->id.'" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Assigner a un employe</button>';
