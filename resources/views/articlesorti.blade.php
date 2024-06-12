bd21ed2e1e974bd8f5b3800a67d6b1603e0d7fb3
eelbe029 <146129886+eelbe029@users.noreply.github.com>
2024-06-12 10:24:33 +0000

    Aestethic stuff on articlesorti.blade.php

--- resources/views/articlesorti.blade.php
+++ resources/views/articlesorti.blade.php
@@ -3,12 +3,8 @@
 @section('content')
 <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h4 class="h2">Articles sorti du stock</h4>
+        <h4 class="h2">Articles sortis du stock</h4>
         <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group me-2">
-                <button type="button" class="btn btn-sm btn-outline-secondary">blank</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">placeholder</button>
-            </div>
             <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center gap-1">
                 placeholder
             </button>
@@ -34,5 +30,5 @@
         </div>
     </div>
 </div>
-<script src="JS/articlesenstock.js"></script>
+<script src="JS/articlesorti.js"></script>
 @endsection
