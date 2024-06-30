8bcffc76135bc62f67b5996db9ff3bc208f03382
eelbe029 <146129886+eelbe029@users.noreply.github.com>
2024-06-30 17:52:51 +0000

    Fixed dashboard functions

--- public/JS/dashboard.js
+++ public/JS/dashboard.js
@@ -7,19 +7,26 @@ window.onload = function () {
         type: "GET",
         url: "graphdisp",
         success: function(response){
-            data = []
-            response.forEach((item,index)=>{
-                data.push({
-                    label: item.name,
-                    y: item.QteDisponible
+            datagraph = []
+            for (let i = 0; i < response.length; i++) {
+                datagraph.push({
+                    label: response[i].name,
+                    y: response[i].QteDisponible
                 })
-            })
+            }
             var options = {
                 animationEnabled: true,
                 title: {
                     text: "Diversite actuelle du stock"
                 },
-                data
+                data: [{
+                    type: "doughnut",
+                    innerRadius: "40%",
+                    showInLegend: true,
+                    legendText: "{label}",
+                    indexLabel: "{label}: #percent%",
+                    dataPoints: datagraph
+                }]
             }
             $("#chartContainer").CanvasJSChart(options);
         }
